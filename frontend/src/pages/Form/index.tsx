736292e76e7dc689f89881bd5cb661fde993f563
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2022-11-03 15:50:43 -0300

    creating repo

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,14 @@
+import { Link, useParams } from 'react-router-dom';
+import { Movie } from 'types/movie';
+import FormCard from 'components/FormCard';
+
+function Form() {
+
+    const params = useParams();
+
+    return (
+        <FormCard movieId={`${params.movieId}`} />
+    );
+}
+
+export default Form;
